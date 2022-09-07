bd7abde4054acf89d32b0d43b4d35c287374f8f7
mahabubr <mahabub.r753@gmail.com>
2022-09-07 22:35:52 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,7 @@
+// ১. setTimeout এর ভিতরে একটা কিছু কনসোল লগ করবে যেটা সাড়ে তিন সেকেন্ড পরে আউটপুট দেখাবে।
+
+// console.log('1st')
+// setTimeout(() => {
+//     console.log('2nd')
+// }, 3500)
+// console.log('3rd')
